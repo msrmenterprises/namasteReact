122dc149992bf600bed84d985b6a09f68aa54d74
Manish <manish@Manishs-Air.attlocal.net>
2024-11-13 10:40:51 -0600

    Chapter 2 Igniting the React App using npm , Parcel Bundler

---
+++ README.md
@@ -0,0 +1,25 @@
+#Parcel 
+- Dev Build 
+- Local Server 
+- HMR = Hot Module Replacement
+- File Watching Algorithm 
+- Caching - Faster Build 
+- Image Optimization 
+- Minification 
+- Bundling 
+- Compress
+- Consistent Hashing 
+- Code Splitting 
+- differential Bundling - Support Older Browser
+- Error Handling 
+- Host Application from http to https 
+- Tree shaking - Remove unused Code 
+-Different Dev and Prod Bundles
+
+
+#Design System 
+-What makes react faster 
+- What makes react app performave 
+- what makes developer experience Better 
+- Learn for system Design 
+
--- app.js
+++ app.js
@@ -1,34 +1,10 @@
-/*
-Creating a Div Parent with Child 
-<div id="parent">
-    <div id="child">
-        <h1>Hello Manish</h1>
-    </div>
-</div>
-
-<div id="parent1">
-    <div id="child1">
-        <h1>Hello Manish</h1>
-        <h1>Hello Child 2</h1>
-    </div>
-</div>
-*/
-
-// const heading = React.createElement(
-//     'h1',
-//     {id:'heading',name:"Manish"},
-//     "Hello World from React")
-
-// const heading  = React.createElement("div",{id:"parent"},
-//     React.createElement('div',{id:"child"},
-//         React.createElement('h1',{},"Hello Manish")
-//     )
-// )
+import React from "react"
+import ReactDOM from 'react-dom/client'
 
 const heading2  = React.createElement('div',{id:'parent1'},
     React.createElement('div',{id:'child1'},[
-        React.createElement('h1',{},'Hello manish'),
-        React.createElement('h1',{},'Hello Child2')
+        React.createElement('h1',{key:"first"},'Hello Manish'),
+        React.createElement('h1',{key:"second"},'Hello Child2')
     ]
     )
 )
